95543c176b4afb303c97128dd195902c00f7281a
96AmEgit <heiyi375@gmail.com>
2026-06-04 22:57:17 +0900

    Create DeliveryUIItem.cs

---
+++ DeliveryUIItem.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+// エラー回避用のダミー
+public class DeliveryUIItem : MonoBehaviour
+{
+    public void Setup(Request r, RequestManager m, DeliveryUIList l) { }
+}
+
+public class ZigZagLayoutGroup : MonoBehaviour
+{
+    public void SetLayoutHorizontal() { }
+    public void SetLayoutVertical() { }
+}
